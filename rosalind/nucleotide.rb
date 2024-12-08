d51ed6d6bcf4a6e02b8a408b494de242c827b967
Rob Cook <message@robcook.me.uk>
2024-12-08 20:21:09 +0000

    Added point mutation count to mixin.

--- rosalind/nucleotide.rb
+++ rosalind/nucleotide.rb
@@ -1,3 +1,5 @@
+require_relative 'sequence_error'
+
 module Rosalind
   module Nucleotide
     def count_a
@@ -20,8 +22,23 @@ module Rosalind
       @sequence.count('U')
     end
 
-    def count_gc
+    def percentage_gc
       (count_g + count_c) / Float(@sequence.length)
+    end
+
+    def point_mutations(comparison)
+      if comparison.sequence.length != @sequence.length
+        raise SequenceError, "Supplied sequence is a different length."
+      end
+
+      count = 0
+
+      @sequence.each_char.with_index do |source_nucleotide, idx|
+        comparison_nucleotide = comparison.sequence[idx]
+        count += 1 if source_nucleotide != comparison_nucleotide
+      end
+
+      count
     end
   end
 end
